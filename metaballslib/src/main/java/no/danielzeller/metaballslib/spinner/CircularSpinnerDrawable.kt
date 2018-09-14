225a10fb0c32ff3ada53dc99a8d68c0ade1bcc2e
Daniel Zeller <>
2018-09-14 18:44:46 +0200

    Created a spinner

---
+++ metaballslib/src/main/java/no/danielzeller/metaballslib/spinner/CircularSpinnerDrawable.kt
@@ -0,0 +1,132 @@
+package no.danielzeller.metaballslib.spinner
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import android.view.animation.Animation
+import android.view.animation.PathInterpolator
+
+
+class CircularSpinnerDrawable(val metaBall: Drawable, val tinColors: IntArray) : Drawable() {
+
+    private val path = Path()
+    private val renderables: ArrayList<Renderable> = ArrayList()
+    private val animations: ArrayList<ValueAnimator> = ArrayList()
+    private val aCoordinates = floatArrayOf(0f, 0f)
+    lateinit var pathMeasure: PathMeasure
+    private val pathOvalRect = RectF();
+    private var circleScale = 0f
+    private var ballSize = 0
+    private var sizeAnim: ValueAnimator? = null
+
+
+    fun startAnimations() {
+        ballSize = (bounds.width() * 0.19f).toInt()
+        animateBallSize(0, ballSize, 300)
+        for (i in 0 until 5) {
+            renderables.add(Renderable(0f, 0f))
+            animateDrawable(i)
+        }
+    }
+
+    fun stopAllAnimations() {
+        for (animation in animations)
+            animation.cancel()
+        animations.clear()
+        sizeAnim?.cancel()
+    }
+
+    fun stopAndHide() {
+        var animatedFractionMin = 100000f
+        var lastRunningAnimIndex = 0
+        for (i in 0 until animations.count()) {
+            animations[i].addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationRepeat(animation: Animator?) {
+                    super.onAnimationRepeat(animation)
+                    animations[i].cancel()
+                }
+            })
+            if (animations[i].animatedFraction > animatedFractionMin) {
+                animatedFractionMin = animations[i].animatedFraction
+                lastRunningAnimIndex = i
+            }
+        }
+        animations[lastRunningAnimIndex].addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                animateBallSize(ballSize, 0, 700)
+            }
+        })
+
+    }
+
+    fun animateBallSize(from: Int, to: Int, duration: Long) {
+        sizeAnim?.cancel()
+        sizeAnim = ValueAnimator.ofInt(from, to).setDuration(duration)
+        sizeAnim?.interpolator = PathInterpolator(.88f, 0f, .15f, 1f)
+        sizeAnim?.addUpdateListener { animation ->
+            ballSize = animation.animatedValue as Int
+            metaBall.setBounds(-ballSize, -ballSize, ballSize, ballSize)
+        }
+        sizeAnim?.start()
+    }
+
+    fun animateDrawable(i: Int) {
+        val anim = ValueAnimator.ofFloat(0f, 1f).setDuration(1800)
+        anim.startDelay = (130 * i).toLong()
+        anim.interpolator = PathInterpolator(.65f, .14f, .17f, 1f)
+        anim.repeatCount = Animation.INFINITE
+        anim.addUpdateListener { animation ->
+            renderables[i].pathPercent = animation.animatedValue as Float
+        }
+        animations.add(anim)
+        anim.start()
+    }
+
+    override fun draw(canvas: Canvas) {
+        canvas.rotate(-90f, bounds.width().toFloat() / 2f, bounds.height().toFloat() / 2f)
+        for (i in 0 until 5) {
+            val renderable = renderables.get(i)
+            pathMeasure.getPosTan(pathMeasure.length * renderable.pathPercent, aCoordinates, null)
+            val count = canvas.save()
+            canvas.translate(aCoordinates[0], aCoordinates[1])
+            metaBall.setTint(tinColors[i])
+            metaBall.draw(canvas)
+            canvas.restoreToCount(count)
+        }
+        invalidateSelf()
+    }
+
+
+    override fun onBoundsChange(bounds: Rect) {
+        super.onBoundsChange(bounds)
+        ballSize = (bounds.width() * 0.19f).toInt()
+        circleScale = bounds.width() - (ballSize * 1.5f)
+        createPath()
+        startAnimations()
+    }
+
+    fun createPath() {
+        pathOvalRect.set(bounds.centerX() - circleScale / 2f, bounds.centerY() - circleScale / 2f, bounds.centerX() + circleScale / 2f, bounds.centerY() + circleScale / 2f)
+        path.reset()
+        path.addOval(pathOvalRect, Path.Direction.CW)
+        pathMeasure = PathMeasure(path, false)
+    }
+
+    override fun setAlpha(alpha: Int) {
+
+    }
+
+    override fun getOpacity(): Int {
+        return PixelFormat.TRANSLUCENT
+    }
+
+    override fun setColorFilter(colorFilter: ColorFilter?) {
+
+    }
+
+
+}
+
+class Renderable(var pathPercent: Float, var scale: Float)
---
+++ metaballslib/src/main/java/no/danielzeller/metaballslib/spinner/Spinner.kt
@@ -0,0 +1,69 @@
+package no.danielzeller.metaballslib.spinner
+
+import android.content.Context
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.widget.FrameLayout
+import android.widget.ImageView
+import no.danielzeller.metaballslib.R
+
+class Spinner : FrameLayout {
+
+    private lateinit var colorArray: IntArray
+    private lateinit var circularSpinnerDrawable: CircularSpinnerDrawable
+
+    constructor(context: Context) : super(context) {
+        loadAttributesFromXml(null)
+        setupBaseViews(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        loadAttributesFromXml(attrs)
+        setupBaseViews(context)
+    }
+
+    private fun loadAttributesFromXml(attrs: AttributeSet?) {
+        val typedArray = context.theme.obtainStyledAttributes(
+                attrs,
+                R.styleable.MetaBallsSpinner,
+                0, 0)
+        try {
+            val colorsArrayID = typedArray.getResourceId(R.styleable.MetaBallsSpinner_colors_array_id, R.array.default_spinner_colors)
+            colorArray = resources.getIntArray(colorsArrayID)
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+    private fun setupBaseViews(context: Context) {
+        val spinnerImageView = ImageView(context)
+        spinnerImageView.setLayerType(View.LAYER_TYPE_HARDWARE, createMetaBallsPaint())
+        circularSpinnerDrawable = CircularSpinnerDrawable(resources.getDrawable(R.mipmap.gradient_oval, null), colorArray)
+        spinnerImageView.setImageDrawable(circularSpinnerDrawable)
+        addView(spinnerImageView, FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT))
+    }
+
+    override fun onVisibilityChanged(changedView: View, visibility: Int) {
+        super.onVisibilityChanged(changedView, visibility)
+        if (visibility == View.VISIBLE) {
+            circularSpinnerDrawable.startAnimations()
+        } else {
+            circularSpinnerDrawable.stopAllAnimations()
+        }
+    }
+
+    private fun createMetaBallsPaint(): Paint {
+        val metaBallsPaint = Paint()
+        metaBallsPaint.setColorFilter(ColorMatrixColorFilter(ColorMatrix(floatArrayOf(
+                1f, 0f, 0f, 0f, 0f,
+                0f, 1f, 0f, 0f, 0f,
+                0f, 0f, 1f, 0f, 0f,
+                0f, 0f, 0f, 150f, -255 * 128f
+        ))))
+        return metaBallsPaint
+    }
+}
